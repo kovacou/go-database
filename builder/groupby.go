55d7f1fa8e5e4206c2060501b43fe580b3149a9a
Alexandre Kovac <contact@kovacou.fr>
2019-09-09 18:21:14 +0200

    add support of ParseGroupBy

--- builder/groupby.go
+++ builder/groupby.go
@@ -9,6 +9,13 @@ const (
 	groupByKeyword = " GROUP BY "
 )
 
+// ParseGroupBy create a new GroupBy based on a string(s) input.
+// This function should be called to initiate the GroupBy field.
+func ParseGroupBy(cols ...string) (out GroupBy) {
+	out.Add(cols...)
+	return
+}
+
 // GroupBy is the representation of the GROUP BY clause.
 type GroupBy struct {
 	Columns
