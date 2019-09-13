28684bbb19ebd159b84bf798d74fba70d1318989
Alexandre Kovac <contact@kovacou.fr>
2019-09-13 18:49:52 +0200

    add support of Update stmt

---
+++ builder/update.go
@@ -0,0 +1,78 @@
+// Copyright © 2019 Alexandre Kovac <contact@kovacou.fr>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package builder
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	updateKeyword = "UPDATE "
+	setKeyword    = " SET "
+)
+
+// Binds is used to bind column to another one. (Raw version, no secure)
+type Binds map[string]string
+
+// String
+func (b Binds) String() string {
+	str := strings.Builder{}
+	return str.String()
+}
+
+// Update is the representation of an Update statement.
+type Update struct {
+	Table  string
+	Values Values
+	Binds  Binds
+	Joins  Joins
+	Where  Where
+}
+
+// String convert the update to string.
+func (u *Update) String() string {
+	q := strings.Builder{}
+	q.WriteString(updateKeyword)
+	q.WriteString(u.Table)
+	q.WriteString(setKeyword)
+
+	// Values
+	{
+		keys := u.Values.Keys()
+		n := len(keys) - 1
+
+		for i, k := range keys {
+			if i < n {
+				fmt.Fprintf(&q, "%s = ?,", k)
+			} else {
+				fmt.Fprintf(&q, "%s = ?", k)
+			}
+		}
+	}
+
+	// Joins section
+	if u.Joins.Len() > 0 {
+		q.WriteString(u.Joins.String())
+	}
+
+	// WHERE clause
+	if u.Where.str.Len() > 0 {
+		q.WriteString(whereKeyword)
+		q.WriteString(u.Where.str.String())
+	}
+	return q.String()
+}
+
+// Args compute the arguments of the update statement.
+func (u *Update) Args() (out []interface{}) {
+	for _, k := range u.Values.Keys() {
+		out = append(out, u.Values[k])
+	}
+
+	out = append(out, u.Where.args...)
+	return
+}
